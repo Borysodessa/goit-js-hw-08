abfe11264bb70fa04c65914655a4736e7c7e23c7
BorysGull <chayka2021@yahoo.com>
2022-08-18 22:25:55 +0300

    make task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const feedBackForm = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const feedbackFormState = {};
+
+
+function getItemLS() {
+  if (localStorage.getItem("feedback-form-state")) {
+    const feedbackFormStateParse = JSON.parse(localStorage.getItem("feedback-form-state"));
+    emailEl.value = feedbackFormStateParse.email;
+    messageEl.value = feedbackFormStateParse.message;
+  }
+}
+
+feedBackForm.addEventListener('input', throttle(handleSetLS, 500));
+
+function handleSetLS(evt) {
+  const evtTargetName = evt.target.name;
+  feedbackFormState[evt.target.name] = evt.target.value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+}
+
+
+feedBackForm.addEventListener('submit', handleClearLS);
+
+function handleClearLS(evt) {
+  evt.preventDefault();
+  console.log(feedbackFormState);
+  localStorage.clear();
+  emailEl.value = ' ';
+  messageEl.value = ' ';
+}
+
+
+
+
+
+
+
+
 
